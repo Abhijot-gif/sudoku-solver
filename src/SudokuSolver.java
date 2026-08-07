cd9eec489694d4a68b617ca9490b6a966c53456c
Abhijot Singh <abhijot.singh16@icloud.com>
2026-08-07 20:31:58 +1000

    Stage 2: add isValid rule checker

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -22,8 +22,42 @@ public class SudokuSolver {
         }
     }
 
+    // Check if the number placed at (row,col) violates the rules of Sudoku.
+    public boolean isValid(int row, int col, int num) {
+        //Rule 1: num must not already be in this current row.
+        for (int c=0; c<9; c++) {
+            if (board[row][c] == num) {
+                return false;
+            }
+        }
+        //Rule 2: num must not already be in this current col.
+        for (int r =0; r<9; r++) {
+            if(board[r][col] == num) {
+                return false;
+            }
+        }
+        //Rule 3: num must not already be in this current 3x3 box.
+        int boxRow = (row/3) * 3; // Top-left row of this cell's box.
+        int boxCol = (col/3) * 3; // Top-left col of this cell's box.
+        for (int r = boxRow; r<boxRow+3; r++) {
+            for (int c = boxCol; c < boxCol + 3; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+        // If all three checks pass:
+        return true;
+    }
+
     public static void main(String[] args) {
         SudokuSolver solver = new SudokuSolver();
         solver.printBoard();
+        System.out.println();
+
+        // Top-left cell is empty [0][2]. Let's test:
+        System.out.println("Can I put 1 at [0][2]? " + solver.isValid(0,2,1));
+        System.out.println("Can I put 5 at [0][2]? " + solver.isValid(0,2,5));
+        System.out.println("Can I put 7 at [0][2]? " + solver.isValid(0,2,7));
     }
 }
